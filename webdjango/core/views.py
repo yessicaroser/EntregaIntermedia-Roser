8734bdbc93bff80535af6c5092be6e66cf0eae3a
Yessica Roser <yessica.roser@gmail.com>
2022-10-28 04:22:11 -0300

    carga de template bootstrap- sb admin

--- webdjango/core/views.py
+++ webdjango/core/views.py
@@ -2,6 +2,43 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+    </ul>
+"""
 
 def home(request):
-    return HttpResponse("<h1>Prueba 1 :D</h1><h2>Portada</h2>")
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Yessica </p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Te brindamos nuestro email y redes sociales para que puedas contactarnos:</p>
+        <ul>
+            <li><a href="mailto:hola@cuentafalsa.com.ar.">Email</a></li>
+            <li><a href="https://github.com/lisasimpson">Github</a></li>
+            <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
+    """)
+
+from django.shortcuts import render, HttpResponse
+
+def home(request):
+     return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
--- webdjango/webdjango/urls.py
+++ webdjango/webdjango/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
 ]
